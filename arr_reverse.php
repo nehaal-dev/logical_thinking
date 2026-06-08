087d3e7c06ce38ad4e76b19fdda99f6f6a8afce7
Nehal Khan <nehalkhan4639@gmail.com>
2026-06-08 23:28:07 +0530

    Array reverse and remove dublicate concept done

--- arr_reverse.php
+++ arr_reverse.php
@@ -8,16 +8,27 @@ error_reporting(E_ALL);
 ini_set('display_errors','ON');
  
 
-$num=[3,24,11,45,24,14,11,22,31,37,39,31,45,24,11,11,10 ,31,39] ;
+$num=[3,24,11,45,24,14,11,22,31,37,39,31,45,24,11,11,10,31,39] ;
 
 $length=count($num);
 
-for($i=$length-1 ; $i >= 0    ; $i--){
+ $uniq=[];
+ 
+ for($i=$length - 1; $i >=0 ; $i-- ){
 
-    $uniq=$num[$i] . ' <br>';
-}
+     if(!isset($uniq[$num[$i]])){
 
-foreach($uniq as $key => $value)
+    $uniq[$num[$i]]=true  ;
+    
+
+     } 
+ }
+ 
+ echo '<pre>' ;
+ print_r($uniq);
+
+
+ 
 
 
 
